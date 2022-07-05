3a45288ccf9f21e743d6de1eb896ad439bab39f8
afam_iz7 <muhammedafham04@gmail.com>
2022-07-05 02:14:11 -0700

    first

--- jb_portal/settings.py
+++ jb_portal/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-)-++!+tvs-&2sl^xkbtvru=4b@@&0+)xmzi_&qe0&!^!$^fr^@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['jbpo.herokuapp.com']
 
 
 # Application definition
